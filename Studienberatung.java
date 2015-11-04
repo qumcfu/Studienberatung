2c61ac9c6454134fa6fd4fd0ac95524376dbb80d
Arne Sibilis <a.sibilis@yahoo.de>
2015-11-04 20:22:18 +0100

    Implemented 'Hast du Fachabitur?'

---
+++ Studienberatung.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Studienberatung {
+
+	public static void main (String[] args){
+		Scanner scanner = new Scanner (System.in);
+		System.out.print ("Hast du Abitur? ");
+		String answer1 = scanner.next ();
+		if (answer1.equalsIgnoreCase ("n")){
+			System.out.print ("Hast du Fachabitur? ");
+		} else {
+
+		}
+	}
+
+
+}
